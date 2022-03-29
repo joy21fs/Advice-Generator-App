7b15f42aff94b5a8f2fc2f6f32efd88dddf7b37b
joy21fs <95619834+joy21fs@users.noreply.github.com>
2022-03-29 18:42:54 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+fetch("https://api.adviceslip.com/advice")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.slip);
+    document.querySelector(
+      ".advice--title"
+    ).innerHTML = `Advice #${data.slip.id}`;
+    document.querySelector(
+      ".advice--content"
+    ).innerHTML = `“${data.slip.advice}”`;
+  });
