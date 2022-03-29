cbbdc90075ecf8dbd68f47a71b566e200c2f0341
joy21fs <95619834+joy21fs@users.noreply.github.com>
2022-03-29 20:19:13 +0800

    add click functionality for fetch

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-fetch("https://api.adviceslip.com/advice")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data.slip);
-    document.querySelector(
-      ".advice--title"
-    ).innerHTML = `Advice #${data.slip.id}`;
-    document.querySelector(
-      ".advice--content"
-    ).innerHTML = `“${data.slip.advice}”`;
-  });
+function getAdvice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.slip);
+      document.querySelector(
+        ".advice--title"
+      ).innerHTML = `Advice #${data.slip.id}`;
+      document.querySelector(
+        ".advice--content"
+      ).innerHTML = `“${data.slip.advice}”`;
+    });
+}
